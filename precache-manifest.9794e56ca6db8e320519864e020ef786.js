3439907d2335a847db6d93e11ebb1d49a19e6d5a
andresponton21 <andresponton21@yahoo.com>
2020-04-21 23:40:41 -0400

    Updates

---
+++ precache-manifest.9794e56ca6db8e320519864e020ef786.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb4689dfccd998d9d62ac13119d28e1b",
+    "url": "/dapp2_project/index.html"
+  },
+  {
+    "revision": "efa3c6f2c6732caa84ac",
+    "url": "/dapp2_project/static/css/main.3a076e4a.chunk.css"
+  },
+  {
+    "revision": "6fd02348ecb7540d43be",
+    "url": "/dapp2_project/static/js/2.ca92ae17.chunk.js"
+  },
+  {
+    "revision": "efa3c6f2c6732caa84ac",
+    "url": "/dapp2_project/static/js/main.4a361c7c.chunk.js"
+  },
+  {
+    "revision": "823e7080a4b7d0fde047",
+    "url": "/dapp2_project/static/js/runtime-main.d1761ea3.js"
+  }
+]);
